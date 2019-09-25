b7d7a7caa27d0f984288615df0742eb9ba3adf41
Victor Avila <victor.avila@venturplex.com>
2019-09-25 12:58:02 -0700

    update dependencies

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -20,7 +20,7 @@ import { AppComponent } from './app.component';
     HttpClientModule,
     MaterialModule,
     UiLoginModule,
-    AppRoutingModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
